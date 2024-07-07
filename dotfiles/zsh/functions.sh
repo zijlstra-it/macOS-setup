e3b7e9306e8e3f0ff3303c1b6b98e4bc343360fc
Sander Zijlstra <sander@zijlstra.it>
2024-07-07 13:57:50 +0200

    Move vv() to functions.sh and minor update to vim aliases

--- dotfiles/zsh/functions.sh
+++ dotfiles/zsh/functions.sh
@@ -1,6 +1,21 @@
-vv() {
-	select config in dev cpow kickstart nvchad; do
-		NVIM_APPNAME=nvim-$config nvim "$@"
-		break
-	done
+# macOS-setup/dotfiles/zsh/functions.sh
+
+# helper function
+exists() {
+	command -v "$1" >/dev/null 2>&1
 }
+
+if exists nvim; then
+	if exists fzf; then
+		vv() {
+			# Assumes all configs exist in directories named ~/.config/nvim*
+			local config=$(find ~/.config -maxdepth 1 -name 'nvim*' | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+
+			# If I exit fzf without selecting a config, don't open Neovim
+			[[ -z $config ]] && echo "No config selected" && return
+
+			# Open Neovim with the selected config
+			NVIM_APPNAME=$(basename "$config") nvim "$@"
+		}
+	fi
+fi
